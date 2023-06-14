e4dfdedce69b3a1e9d513e70f066e29064716828
Evelina <evelina.venckute@swedbank.lt>
2023-06-14 14:28:56 +0300

    added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   templateUrl: './dashboard.component.html',
+  imports: [RouterModule]
+
 })
 export class DashboardComponent {}
